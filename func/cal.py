3b14527c7af2b5451de533a555a460e7807c1e0b
czw <83765680+czw1296924847@users.noreply.github.com>
2022-03-24 18:30:30 +0800

    Add files via upload

---
+++ func/cal.py
@@ -0,0 +1,338 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric.nn as gnn
+import pandas as pd
+from sklearn.metrics import r2_score
+from torch.utils.data import Dataset
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+
+def eval_on_features(regress, x_train, y_train, x_test, y_test, title, fig_size=(12, 8), alpha=0.5):
+    num_train, num_test = x_train.shape[0], x_test.shape[0]
+    num = num_train + num_test
+    regress.fit(x_train, y_train.reshape(-1))
+    y_train_pred = regress.predict(x_train)
+    y_test_pred = regress.predict(x_test)
+
+    rmse_train = get_rmse(a1=y_train, a2=y_train_pred)
+    rmse_test = get_rmse(a1=y_test, a2=y_test_pred)
+    r2_train = get_r2_score(y_train_pred, y_train)
+    r2_test = get_r2_score(y_test_pred, y_test)
+
+    plt.figure(figsize=fig_size)
+    # plt.title(title, fontsize=30)
+    range_train = np.arange(num_train)
+    range_test = np.arange(num_train, num)
+    plt.plot(range_train, y_train, label="$y^{train}$", alpha=alpha)
+    plt.plot(range_train, y_train_pred, label="$\hat{y}^{train}$", alpha=alpha)
+    plt.plot(range_test, y_test, label="$y^{test}$", alpha=alpha)
+    plt.plot(range_test, y_test_pred, label="$\hat{y}^{test}$", alpha=alpha)
+    plt.legend(fontsize=25, loc=1)
+    plt.ylabel("Value", fontsize=30)
+    plt.xlabel("Time", fontsize=30)
+    return rmse_train, rmse_test, r2_train, r2_test
+
+
+def create_inout_sequences(input_data, x_length=32, y_length=4, style="list", ml_dim=0, ld1=False):
+    seq_list, seq_arr, label_arr = [], None, None
+    data_length = len(input_data)
+    x_y_length = x_length + y_length
+    if style == "list":
+        for i in range(data_length - x_y_length + 1):
+            if input_data.ndim == 2:
+                seq = input_data[i: (i + x_length), :]
+                if ld1:
+                    label = input_data[(i + x_length): (i + x_length + y_length), ml_dim]
+                else:
+                    label = input_data[(i + x_length): (i + x_length + y_length), :]
+            elif input_data.ndim == 1:
+                seq = input_data[i: (i + x_length)]
+                label = input_data[(i + x_length): (i + x_length + y_length)]
+            elif input_data.ndim == 3:
+                seq = input_data[i: (i + x_length), :, :]
+                if ld1:
+                    label = input_data[(i + x_length): (i + x_length + y_length), :, ml_dim]
+                else:
+                    label = input_data[(i + x_length): (i + x_length + y_length), :, :]
+            seq_list.append((seq, label))
+        return seq_list
+
+    elif style == "arr":
+        for i in range(data_length - x_y_length + 1):
+            if input_data.ndim == 2:
+                seq = input_data[i: (i + x_length), :]
+                label = input_data[(i + x_length): (i + x_length + y_length), ml_dim].reshape(1, -1)
+                seq = np.expand_dims(seq, 0)
+            elif input_data.ndim == 1:
+                seq = input_data[i: (i + x_length)]
+                label = input_data[(i + x_length): (i + x_length + y_length)]
+                seq, label = seq.reshape(1, -1), label.reshape(1, -1)
+            if (seq_arr is None) & (label_arr is None):
+                seq_arr, label_arr = seq, label
+            else:
+                seq_arr, label_arr = np.vstack([seq_arr, seq]), np.vstack([label_arr, label])
+        return seq_arr, label_arr
+
+
+def index_to_mask(index, size):
+    mask = torch.zeros((size, ), dtype=torch.bool)
+    mask[index] = 1
+    return mask
+
+
+class GNNTime(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, edge_weight, gnn_style, num_layers=1):
+        super(GNNTime, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.edge_weight = nn.Parameter(edge_weight)
+        self.gnn_style = gnn_style
+        self.num_layers = num_layers
+        self.pre = nn.Sequential(nn.ReLU(), nn.Dropout())
+        self.gcn1 = gnn.GCNConv(input_dim, hidden_dim)
+        self.gcn2 = gnn.GCNConv(hidden_dim, output_dim)
+        self.che1 = gnn.ChebConv(input_dim, hidden_dim, K=2)
+        self.che2 = gnn.ChebConv(hidden_dim, output_dim, K=2)
+        self.sage1 = gnn.SAGEConv(input_dim, hidden_dim)
+        self.sage2 = gnn.SAGEConv(hidden_dim, output_dim)
+        self.gin1 = gnn.GraphConv(input_dim, hidden_dim)
+        self.gin2 = gnn.GraphConv(hidden_dim, output_dim)
+        self.tran1 = gnn.TransformerConv(input_dim, hidden_dim)
+        self.tran2 = gnn.TransformerConv(hidden_dim, output_dim)
+        self.agnn1 = gnn.AGNNConv(input_dim, hidden_dim)
+        self.agnn2 = gnn.AGNNConv(hidden_dim, output_dim)
+        self.tag1 = gnn.TAGConv(input_dim, hidden_dim)
+        self.tag2 = gnn.TAGConv(hidden_dim, output_dim)
+        self.arma1 = gnn.ARMAConv(input_dim, hidden_dim)
+        self.arma2 = gnn.ARMAConv(hidden_dim, output_dim)
+        self.lstm1 = nn.LSTM(hidden_dim, hidden_dim, num_layers, batch_first=True)
+        self.lstm2 = nn.LSTM(hidden_dim, hidden_dim, num_layers, batch_first=True)
+        self.gru1 = nn.GRU(hidden_dim, hidden_dim, num_layers, batch_first=True)
+        self.gru2 = nn.GRU(hidden_dim, hidden_dim, num_layers, batch_first=True)
+        self.cnn1 = nn.Conv2d(1, 32, kernel_size=(3, 3), padding=(1, 1))
+        self.cnn2 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=(1, 1))
+        self.cnn3 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=(1, 1))
+        self.cnn4 = nn.Conv2d(32, 1, kernel_size=(3, 3), padding=(1, 1))
+        self.bn = nn.BatchNorm2d(32)
+
+    def forward(self, x, edge_index):
+        if self.gnn_style == "gcn":             # Graph Convolution Network Model
+            h = self.gcn1(x, edge_index, self.edge_weight)
+            h = self.pre(h)
+            h = self.gcn2(h, edge_index, self.edge_weight)
+        elif self.gnn_style == "che":           # Chebyshev Network
+            h = self.che1(x, edge_index)
+            h = self.pre(h)
+            h = self.che2(h, edge_index)
+        elif self.gnn_style == "sage":          # GraphSAGE Model
+            h = self.sage1(x, edge_index)
+            h = self.pre(h)
+            h = self.sage2(h, edge_index)
+        elif self.gnn_style == "gin":           # Graph Isomorphic Network Model
+            h = self.gin1(x, edge_index, self.edge_weight)
+            h = self.pre(h)
+            h = self.gin2(h, edge_index, self.edge_weight)
+        elif self.gnn_style == "tran":
+            h = self.tran1(x, edge_index)
+            h = self.pre(h)
+            h = self.tran2(h, edge_index)
+        elif self.gnn_style == "agnn":
+            h = self.agnn1(x, edge_index)
+            h = self.pre(h)
+            h = self.agnn2(h, edge_index)
+        elif self.gnn_style == "tag":
+            h = self.tag1(x, edge_index, self.edge_weight)
+            h = self.pre(h)
+            h = self.tag2(h, edge_index, self.edge_weight)
+        elif self.gnn_style == "arma":
+            h = self.arma1(x, edge_index, self.edge_weight)
+            h = self.pre(h)
+            h = self.arma2(h, edge_index, self.edge_weight)
+        elif (self.gnn_style == "sage_lstm") | (self.gnn_style == "sage_gru") | (self.gnn_style == "sage_res"):
+            h = self.sage1(x, edge_index)
+            h = self.pre(h)
+            h = h.unsqueeze(0)
+            if self.gnn_style == "sage_lstm":
+                h, (_, _) = self.lstm1(h)
+                h, (_, _) = self.lstm2(self.pre(h))
+            elif self.gnn_style == "sage_gru":
+                h, _ = self.gru1(h)
+                h, _ = self.gru2(self.pre(h))
+            elif self.gnn_style == "sage_res":          # ResGraphNet
+                h = h.unsqueeze(0)
+                out = self.cnn1(h)
+                out_0 = out
+                out = self.cnn2(self.pre(out))
+                out = self.cnn3(self.pre(out))
+                out = out + out_0
+                out = self.cnn4(self.pre(out))
+                h = out.squeeze(0)
+            h = h.squeeze(0)
+            h = self.pre(h)
+            h = self.sage2(h, edge_index)
+        else:
+            raise TypeError("{} is unknown for gnn style".format(self.gnn_style))
+        return h
+
+
+def path_graph(m):
+    adm = np.zeros(shape=(m, m))
+    for i in range(m - 1):
+        adm[i, i + 1] = 1
+    return adm
+
+
+def tran_adm_to_edge_index(adm):
+    if isinstance(adm, np.ndarray):
+        u, v = np.nonzero(adm)
+        num_edges = u.shape[0]
+        edge_index = np.vstack([u.reshape(1, -1), v.reshape(1, -1)])
+        edge_weight = np.zeros(shape=u.shape)
+        for i in range(num_edges):
+            edge_weight_one = adm[u[i], v[i]]
+            edge_weight[i] = edge_weight_one
+        edge_index = torch.from_numpy(edge_index.astype(np.int64))
+        edge_weight = torch.from_numpy(edge_weight.astype(np.float32))
+        return edge_index, edge_weight
+    elif torch.is_tensor(adm):
+        u, v = torch.nonzero(adm, as_tuple=True)
+        num_edges = u.shape[0]
+        edge_index = torch.cat((u.view(1, -1), v.view(1, -1)))
+        edge_weight = torch.zeros(size=u.shape)
+        for i in range(num_edges):
+            edge_weight[i] = adm[u[i], v[i]]
+        return edge_index, edge_weight
+
+
+def get_rmse(a1, a2):
+    a1, a2 = a1.reshape(-1), a2.reshape(-1)
+    m_1, m_2 = a1.shape[0], a2.shape[0]
+    if m_1 < m_2:
+        m = m_1
+    else:
+        m = m_2
+    a1_m, a2_m = a1[:m], a2[:m]
+    result = np.sqrt(np.sum(np.square(a1_m - a2_m)) / m)
+    return result
+
+
+def plot_2curve(a1, a2, l1=None, l2=None, title=None, x_name=None, y_name=None, alpha=0.5, fig_size=(12, 12),
+                font_size=20, font_size_le=15, style='plot', s=10, marker=False):
+    plt.figure(figsize=fig_size)
+    if style == 'stem':
+        plt.stem(a1, label=l1, alpha=alpha)
+        plt.stem(a2, label=l2, alpha=alpha)
+    elif style == 'plot':
+        if marker:
+            plt.plot(a1, label=l1, marker="o", markersize=10, alpha=alpha)
+            plt.plot(a2, label=l2, marker="o", markersize=10, alpha=alpha)
+        else:
+            plt.plot(a1, label=l1, alpha=alpha)
+            plt.plot(a2, label=l2, alpha=alpha)
+    elif style == 'scatter':
+        plt.scatter(x=np.linspace(1, a1.shape[0], a1.shape[0]), y=a1, label=l1, s=s)
+        plt.scatter(x=np.linspace(1, a2.shape[0], a2.shape[0]), y=a2, label=l2, s=s)
+    plt.xlabel(x_name, fontsize=font_size)
+    plt.ylabel(y_name, fontsize=font_size)
+    if (l1 is None) & (l2 is None):
+        pass
+    else:
+        plt.legend(fontsize=font_size_le)
+    plt.title(title, fontsize=font_size)
+    return None
+
+
+def plot_distribute(x, bins, jump, title=None, x_name=None, y_name="Frequency"):
+    if x.ndim == 1:
+        pass
+    elif (x.ndim == 2) & (x.shape[1] == 1):
+        x = x.reshape(-1)
+    else:
+        raise TypeError("x must be 1d or row vector of 2d, but got {}d".format(x.shape[1]))
+    x_label, x_bins = pd.cut(x, bins=bins, retbins=True)
+    x_label = pd.DataFrame(x_label)
+    x_label_vc = pd.DataFrame(x_label).value_counts()
+    interval = x_label_vc.index.tolist()
+    interval_sum = x_label_vc.values
+    mid_all = []
+    for i in range(bins):
+        interval_one = interval[i][0]
+        mid = (interval_one.left + interval_one.right) / 2
+        mid_all.append(mid)
+    mid_all = np.array(mid_all)
+    sort_index = np.argsort(mid_all)
+    mid_all_sort = mid_all[sort_index]
+    mid_all_sort = np.around(mid_all_sort, 2)
+    interval_sum_sort = interval_sum[(sort_index)]
+
+    plt.figure(figsize=(12, 12))
+    if title is not None:
+        plt.title(title, fontsize=30)
+    plt.bar(x=np.arange(bins), height=interval_sum_sort)
+    labels = mid_all_sort[np.arange(0, bins, jump)]
+    ticks = np.arange(bins)[np.arange(0, bins, jump)]
+    plt.xticks(ticks=ticks, labels=labels)
+    if x_name is not None:
+        plt.xlabel(x_name, fontsize=30)
+    if y_name is not None:
+        plt.ylabel(y_name, fontsize=30)
+    return None
+
+
+# 计算真实标签与预测结果的r2分数
+def get_r2_score(output, y):
+    if (type(output) is np.ndarray) & (type(y) is np.ndarray):      # numpy数组类型
+        output = output.reshape(-1)
+        y = y.reshape(-1)
+    elif (torch.is_tensor(output)) & (torch.is_tensor(y)):          # pytorch张量类型
+        output = output.detach().cpu().numpy().reshape(-1)
+        y = y.detach().cpu().numpy().reshape(-1)
+    else:
+        raise TypeError("type of output and y must be the same, but got {} and {}".format(type(output), type(y)))
+    r2 = r2_score(y, output)
+    return r2
+
+
+class ResNet(nn.Module):
+    def __init__(self, hidden_dim, output_dim, x_length):
+        super(ResNet, self).__init__()
+        self.lin_pre = nn.Linear(1, hidden_dim)
+        self.cnn1 = nn.Conv2d(1, 32, kernel_size=(3, 3), padding=(1, 1))
+        self.cnn2 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=(1, 1))
+        self.cnn3 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=(1, 1))
+        self.cnn4 = nn.Conv2d(32, 1, kernel_size=(3, 3), padding=(1, 1))
+        self.last1 = nn.Linear(hidden_dim, 1)
+        self.last2 = nn.Linear(x_length, output_dim)
+        self.pre = nn.Sequential(nn.ReLU(), nn.Dropout())
+
+    def forward(self, x):
+        h = x.unsqueeze(1).unsqueeze(3)
+        h = self.lin_pre(h)
+        h = self.cnn1(h)
+        h_0 = h
+        h = self.cnn2(self.pre(h))
+        h = self.cnn3(self.pre(h))
+        h = h + h_0
+        h = self.cnn4(self.pre(h))
+        h = self.last1(self.pre(h)).squeeze(3).squeeze(1)
+        h = self.last2(h)
+        return h
+
+
+class MyData(Dataset):
+    def __init__(self, x, y):
+        super(MyData, self).__init__()
+        self.x = torch.from_numpy(x).float()
+        self.y = torch.from_numpy(y).float()
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, idx):
+        x_one = self.x[idx, :]
+        y_one = self.y[idx, :]
+        return x_one, y_one
---
+++ rnn_gnn_machine.py
@@ -0,0 +1,314 @@
+"""
+Using RNN, GNN and ML to predict the time series respectively
+"""
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+import os.path as osp
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import DataLoader, Dataset
+from func.cal import path_graph, create_inout_sequences, tran_adm_to_edge_index, index_to_mask, GNNTime,\
+    eval_on_features, get_rmse, plot_2curve, plot_distribute, get_r2_score, ResNet, MyData
+
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+x_length = 60                   # Data sequence length
+y_length = 1                    # Label sequence length
+lr_gnn = 0.0001                  # Gnn Learning rate
+lr_res = 0.0001                   # Rnn Learning rate
+weight_decay = 5e-4
+epochs_gnn = 4000
+epochs_res = 50
+hidden_dim = 64
+gnn_style_all = ["gcn", "che", "sage", "gin", "tran", "tag", "sage_res"]
+gnn_style = "sage"               # GNN style
+num_layers = 1
+save_fig = True                  # If Save picture
+save_txt = False
+ratio_train = 0.5               # Proportion of training datasets
+fig_size = (12, 12)
+ts_name_all = ["seismic_trace", "electricity", "sales", "solar", "pm25", "traffic", "temperature", "WellData"]
+ts_name_folder = "temperature"    # Name of the folder where the data resides
+ts_name = "Afghanistan"       # Name of data
+batch_size = 32
+
+address = os.getcwd()
+x_address = osp.join(address, "datasets", ts_name_folder, ts_name + ".npy")
+x = np.load(x_address)
+num = x.shape[0]
+
+result_address = osp.join(address, "result", ts_name)
+if not(osp.exists(result_address)):
+    os.makedirs(result_address)
+
+num_train = int(ratio_train * num)
+data_train, data_test = x[:num_train], x[num_train:num]
+
+# scale = MinMaxScaler()
+# scale.fit(data_train)
+# data_train = scale.transform(data_train)
+# data_test = scale.transform(data_test)
+
+"""
+Using Graph Neural network, predicting time series
+"""
+print("\nRunning, GNN or ResGraphNet")
+x_train, y_train = create_inout_sequences(data_train, x_length, y_length, style="arr")
+x_test, y_test = create_inout_sequences(data_test, x_length, y_length, style="arr")
+
+x_train = torch.from_numpy(x_train).float().to(device)
+x_test = torch.from_numpy(x_test).float().to(device)
+y_train = torch.from_numpy(y_train).float().to(device)
+y_test = torch.from_numpy(y_test).float().to(device)
+num_nodes = x_train.shape[0] + x_test.shape[0]
+
+x = torch.cat((x_train, x_test), dim=0)
+y = torch.cat((y_train, y_test), dim=0)
+
+adm = path_graph(num_nodes)
+edge_index, edge_weight = tran_adm_to_edge_index(adm)
+
+train_index = torch.arange(num_train, dtype=torch.long)
+test_index = torch.arange(num_train, num_nodes, dtype=torch.long)
+train_mask = index_to_mask(train_index, num_nodes).to(device)
+test_mask = index_to_mask(test_index, num_nodes).to(device)
+
+model = GNNTime(x_length, hidden_dim, y_length, edge_weight, gnn_style, num_layers).to(device)
+criterion = torch.nn.MSELoss().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr_gnn, weight_decay=weight_decay)
+edge_index = edge_index.to(device)
+
+print("Running, {}".format(gnn_style))
+for epoch in range(epochs_gnn):
+    model.train()
+    optimizer.zero_grad()
+    output = model(x, edge_index)
+    output_train, y_train = output[train_mask], y[train_mask]
+    train_loss = criterion(output_train, y_train)
+    train_loss.backward()
+    optimizer.step()
+
+    model.eval()
+    output_test, y_test = output[test_mask], y[test_mask]
+    test_loss = criterion(output_test, y_test)
+
+    r2_train = get_r2_score(output_train, y_train)
+    r2_test = get_r2_score(output_test, y_test)
+
+    if (epoch + 1) % 100 == 0:
+        print("Epoch: {:05d}  Loss_Train: {:.5f}  Loss_Test: {:.5f}  R2_Train: {:.7f}  R2_Test: {:.7f}".
+              format(epoch + 1, train_loss.item(), test_loss.item(), r2_train, r2_test))
+
+train_true = y_train.detach().cpu().numpy()[:, 0]
+train_predict = output_train.detach().cpu().numpy()[:, 0]
+test_true = y_test.detach().cpu().numpy()[:, 0]
+test_predict = output_test.detach().cpu().numpy()[:, 0]
+
+rmse_train = get_rmse(train_true, train_predict)
+rmse_test = get_rmse(test_true, test_predict)
+
+r2_train_gnn = get_r2_score(train_true, train_predict)
+r2_test_gnn = get_r2_score(test_true, test_predict)
+
+e_gnn = test_true - test_predict
+plot_distribute(e_gnn, 40, 4, x_name="e")
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+if save_fig:
+    plt.savefig(osp.join(result_address, ts_name + "_" + gnn_style + "_error_distribution.png"))
+
+plt.figure(figsize=fig_size)
+x_lim_train = np.arange(num_train)
+x_lim_test = np.arange(num_train, num_nodes)
+plt.plot(x_lim_train, train_true, label="$y^{train}$", alpha=0.5)
+plt.plot(x_lim_train, train_predict, label="$\hat{y}^{train}$", alpha=0.5)
+plt.plot(x_lim_test, test_true, label="$y^{test}$", alpha=0.5)
+plt.plot(x_lim_test, test_predict, label="$\hat{y}^{test}$", alpha=0.5)
+plt.xlabel("Time", fontsize=30)
+plt.ylabel("Value", fontsize=30)
+plt.legend(fontsize=25)
+# title = "{}, {}".format(ts_name, gnn_style)
+# plt.title(title, fontsize=30)
+if save_fig:
+    plt.savefig(osp.join(result_address, ts_name + "_" + gnn_style + ".png"))
+print("{}:  RMSE_Train={:.5f}  RMSE_Test={:.5f}  R2_Train={:.7f}  R2_Test={:.7f}".
+      format(gnn_style, rmse_train, rmse_test, r2_train, r2_test))
+
+"""
+Using machine learning to predict time series
+"""
+print("\nRunning, Machine Learning")
+x_train, y_train = create_inout_sequences(data_train, x_length, y_length, style="arr")         # 训练集序列
+x_test, y_test = create_inout_sequences(data_test, x_length, y_length, style="arr")           # 测试集序列
+
+forest = RandomForestRegressor()
+linear = LinearRegression()
+svr = SVR()
+sgd = SGDRegressor()
+
+rmse_train_forest, rmse_test_forest, r2_train_forest, r2_test_forest = eval_on_features(
+    forest, x_train, y_train, x_test, y_test, ts_name + ", Random Forest", fig_size)
+print("forest:  RMSE_Train: {:.5f}  RMSE_Test: {:.5f}  R2_Train: {:.6f}  R2_Test: {:.6f}".
+      format(rmse_train_forest, rmse_test_forest, r2_train_forest, r2_test_forest))
+if save_fig:
+    plt.savefig(osp.join(result_address, ts_name + "_Forest.png"))
+
+rmse_train_linear, rmse_test_linear, r2_train_linear, r2_test_linear = eval_on_features(
+    linear, x_train, y_train, x_test, y_test, ts_name + ", Linear Regression", fig_size)
+print("linear:  RMSE_Train: {:.5f}  RMSE_Test: {:.5f}  R2_Train: {:.6f}  R2_Test: {:.6f}".
+      format(rmse_train_linear, rmse_test_linear, r2_train_linear, r2_test_linear))
+if save_fig:
+    plt.savefig(osp.join(result_address, ts_name + "_Linear.png"))
+
+e_linear = test_true - test_predict
+plot_distribute(e_linear, 40, 4, x_name="e")
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+if save_fig:
+    plt.savefig(osp.join(result_address, ts_name + "_Linear_error_distribution.png"))
+
+rmse_train_svr, rmse_test_svr, r2_train_svr, r2_test_svr = eval_on_features(
+    svr, x_train, y_train, x_test, y_test, ts_name + ", Support Vector Regression", fig_size)
+print("svr:  RMSE_Train: {:.5f}  RMSE_Test: {:.5f}  R2_Train: {:.6f}  R2_Test: {:.6f}".
+      format(rmse_train_svr, rmse_test_svr, r2_train_svr, r2_test_svr))
+if save_fig:
+    plt.savefig(osp.join(result_address, ts_name + "_SVR.png"))
+
+rmse_train_sgd, rmse_test_sgd, r2_train_sgd, r2_test_sgd = eval_on_features(
+    sgd, x_train, y_train, x_test, y_test, ts_name + "，Random Gradient Descent", fig_size)
+print("sgd:  RMSE_Train: {:.5f}  RMSE_Test: {:.5f}  R2_Train: {:.6f}  R2_Test: {:.6f}".
+      format(rmse_train_sgd, rmse_test_sgd, r2_train_sgd, r2_test_sgd))
+if save_fig:
+    plt.savefig(osp.join(result_address, ts_name + "_RGD.png"))
+
+"""
+Using ResNet to predict time series
+"""
+print("\nRunning, ResNet")
+x_train, y_train = create_inout_sequences(data_train, x_length, y_length, style="arr")
+x_test, y_test = create_inout_sequences(data_test, x_length, y_length, style="arr")
+
+train_dataset = MyData(x_train, y_train)
+test_dataset = MyData(x_test, y_test)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+model = ResNet(hidden_dim, y_length, x_length).to(device)
+criterion = torch.nn.MSELoss().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr_res, weight_decay=weight_decay)
+
+for epoch in range(epochs_res):
+    loss_train_all, loss_test_all = 0, 0
+    train_true, train_predict, test_true, test_predict = [], [], [], []
+    for idx, (x_train, y_train) in enumerate(train_loader):
+        x_train, y_train = x_train.to(device), y_train.to(device)
+        optimizer.zero_grad()
+        output_train = model(x_train)
+        loss_train = criterion(output_train, y_train)
+        loss_train.backward()
+        optimizer.step()
+        loss_train_all = loss_train_all + loss_train.item()
+
+        train_predict_one = output_train.detach().cpu().numpy()[:, 0]
+        train_true_one = y_train.detach().cpu().numpy()[:, 0]
+        if idx == 0:
+            train_true = train_true_one
+            train_predict = train_predict_one
+        else:
+            train_true = np.concatenate((train_true, train_true_one), axis=0)
+            train_predict = np.concatenate((train_predict, train_predict_one), axis=0)
+
+    for idx, (x_test, y_test) in enumerate(test_loader):
+        x_test, y_test = x_test.to(device), y_test.to(device)
+        output_test = model(x_test)
+        loss_test = criterion(output_test, y_test)
+        loss_test_all = loss_test_all + loss_test.item()
+
+        test_predict_one = output_test.detach().cpu().numpy()[:, 0]
+        test_true_one = y_test.detach().cpu().numpy()[:, 0]
+        if idx == 0:
+            test_true = test_true_one
+            test_predict = test_predict_one
+        else:
+            test_true = np.concatenate((test_true, test_true_one), axis=0)
+            test_predict = np.concatenate((test_predict, test_predict_one), axis=0)
+
+    rmse_train = get_rmse(train_predict, train_true)
+    rmse_test = get_rmse(test_predict, test_true)
+    r2_train = get_r2_score(train_predict, train_true)
+    r2_test = get_r2_score(test_predict, test_true)
+    print("Epoch: {:04d}  RMSE_Train: {:.7f}  RMSE_Test: {:.7f}  R2_Train: {:.7f}  R2_Test: {:.7f}".
+          format(epoch, rmse_train, rmse_test, r2_train, r2_test))
+
+rmse_train_res = get_rmse(train_predict, train_true)
+rmse_test_res = get_rmse(test_predict, test_true)
+r2_train_res = get_r2_score(train_predict, train_true)
+r2_test_res = get_r2_score(test_predict, test_true)
+
+e_res = test_true - test_predict
+plot_distribute(e_res, 40, 4, x_name="e")
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+if save_fig:
+    plt.savefig(osp.join(result_address, ts_name + "_ResNet_error_distribution.png"))
+
+plt.figure(figsize=fig_size)
+len_train, len_test = train_true.shape[0], test_true.shape[0]
+train_lim, test_lim = np.arange(len_train), np.arange(len_train, len_train + len_test)
+plt.plot(train_lim, train_true, label="$y^{train}$", alpha=0.5)
+plt.plot(train_lim, train_predict, label="$\hat{y}^{train}$", alpha=0.5)
+plt.plot(test_lim, test_true, label="$y^{test}$", alpha=0.5)
+plt.plot(test_lim, test_predict, label="$\hat{y}^{test}$", alpha=0.5)
+# plt.title("{}， ResNet".format(ts_name), fontsize=30)
+plt.legend(fontsize=25)
+plt.xlabel("Time", fontsize=30)
+plt.ylabel("Value", fontsize=30)
+if save_fig:
+    plt.savefig(osp.join(result_address, ts_name + "_ResNet.png"))
+print("ResNet:  RMSE_Train: {:.5f}  RMSE_Test: {:.5f}  R2_Train: {:.6f}  R2_Test: {:.6f}".
+      format(rmse_train_res, rmse_test_res, r2_train_res, r2_test_res))
+
+
+# 将各个模型输出结果，追加写入文件
+if save_txt:
+    info_txt_address = osp.join(result_address, "../result.txt")  # 保存参数信息的txt文件地址
+    info_df_address = osp.join(result_address, "../result.csv")  # 保存参数信息的csv文件地址
+    f = open(info_txt_address, 'a')
+    if osp.getsize(info_txt_address) == 0:          # 若此前未曾运行，在文本第一行加入各个特征的名称
+        f.write("ts_name gnn_style r2_test_gnn r2_test_res r2_test_forest r2_test_linear r2_test_svr r2_test_sgd "
+                "hidden_dim batch_size num_layers epochs_gnn lr_gnn epochs_res lr_res\n")
+    f.write(str(ts_name) + "\t")
+    f.write(str(gnn_style) + "\t")
+    f.write(str(r2_test_gnn) + "\t")
+    f.write(str(r2_test_res) + "\t")
+    f.write(str(r2_test_forest) + "\t")
+    f.write(str(r2_test_linear) + "\t")
+    f.write(str(r2_test_svr) + "\t")
+    f.write(str(r2_test_sgd) + "\t")
+    f.write(str(hidden_dim) + "\t")
+    f.write(str(batch_size) + "\t")
+    f.write(str(num_layers) + "\t")
+    f.write(str(epochs_gnn) + "\t")
+    f.write(str(lr_gnn) + "\t")
+    f.write(str(epochs_res) + "\t")
+    f.write(str(lr_res) + "\t")
+
+    f.write("\n")                           # 准备下一次运行
+    f.close()                               # 关闭文件
+
+    # 保存为dataframe格式
+    info = np.loadtxt(info_txt_address, dtype=str)
+    columns = info[0, :].tolist()           # 特征的名称，作为列的名称
+    values = info[1:, :]                    # 特征的取值
+    info_df = pd.DataFrame(values, columns=columns)
+    info_df.to_csv(info_df_address)
+
+plt.show()
